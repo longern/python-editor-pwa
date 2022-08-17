816a6d4cea158eb6d2be1e1afc27a92336bd7d22
longern <771815005@qq.com>
2022-08-17 15:58:36 +0800

    Use PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: "Python Editor PWA",
+      },
+    }),
+  ],
+});
