7cab861dc24af2f10d1e4f984c27f5ee2e00e18a
longern <771815005@qq.com>
2022-08-17 13:51:17 +0800

    Initialize python editor

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { basicSetup, EditorView } from "codemirror";
+import { python } from "@codemirror/lang-python";
+
+new EditorView({
+  extensions: [basicSetup, python()],
+  parent: document.getElementById("app") as HTMLElement,
+});
+</script>
+
+<template></template>
+
+<style>
+.cm-editor {
+  width: 100%;
+  height: 100vh;
+}
+</style>
