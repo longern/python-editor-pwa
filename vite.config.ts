159120933d857d9d393062b12f8373b311ca3de5
longern <771815005@qq.com>
2022-08-19 15:41:57 +0800

    Enable auto update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
+      registerType: "autoUpdate",
       manifest: {
         name: "Python Editor PWA",
         short_name: "PyEditor",
